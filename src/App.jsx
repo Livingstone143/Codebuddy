8e8b2c99f732a6a613ae61fd62465ca083b152c8
Livingstone143 <paramalla2004@gmail.com>
2024-08-02 23:42:58 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   
+          <h1 className="logo react logo-spin" >hi</h1>
+       
+  )
+}
+
+export default App
